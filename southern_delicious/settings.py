ab0301ffe209ee4d781a20a4a93729f92776be5d
EC2 Default User <ec2-user@ip-172-31-2-172.ec2.internal>
2024-03-29 00:10:06 +0000

    changes in the setting.py string

--- southern_delicious/settings.py
+++ southern_delicious/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v7)ouo@=dhswvnfs!@o$v07^m^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
